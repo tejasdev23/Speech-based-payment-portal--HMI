12a895016a82f14978702be5a9bdc85853f75b9d
Tejas Patel <tejasdev23@gmail.com>
2018-04-13 14:15:47 +0530

    initial and final commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/THEORLAN2/pen/mPLPwj.
+
+ A single form to sign and to register. - Click on the tab Sign Up and Sign In to view the Effect
---
+++ payment.js
@@ -0,0 +1,27 @@
+
+function pincheck()
+{
+  
+    // var x = document.myform.couponCode.value;
+    // var y=document.myform.cardNumber.value;
+    // var z=document.myform.CVV.value;
+
+    // if(x.length!=4)
+    // {
+    //     alert("Enter a valid 4 digit PIN"); 
+    // }
+    // if(y.length!=16)
+    // {
+    //     alert("Invalid card NO");
+    // }
+    // if(z.length!=3)
+    // {
+    //     alert("Invalid CVV no");
+    // }
+    // console.log(x);
+    // console.log(y);
+    // console.log(z);
+    console.log('h');
+    window.location.href="load.html";
+}
+
